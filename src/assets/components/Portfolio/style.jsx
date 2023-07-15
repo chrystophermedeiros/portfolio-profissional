17815aaf54ff509e00b45dd49e746116914ed487
chrystophermedeiros <chrystopher312@gmail.com>
2023-07-15 19:58:05 -0300

    ajuste no portfolio para mobile

--- src/assets/components/Portfolio/style.jsx
+++ src/assets/components/Portfolio/style.jsx
@@ -7,14 +7,13 @@ export const Container = styled.section`
 
     
     .rec.rec-arrow {
-    border-radius: 0;
     background-color: #9758a6;
     color: #ffffff;
     border-radius: 50px;
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
     @media (max-width: 994px) {
         display: none;
-}
+    }
 
    
   }
@@ -29,6 +28,10 @@ export const Container = styled.section`
     border: none;
     background-color: transparent;
   }
+
+
+ 
+ 
 
     h1 {
         text-align: center;
@@ -80,13 +83,8 @@ export const Card = styled.div`
     padding: 10px;
     margin: 5px;
     @media (max-width: 994px) {
-
-
-    img{
-
-    width: 80%;
-
-    }
+        width: 300px;
+        height: 300px;
 }
 
 
