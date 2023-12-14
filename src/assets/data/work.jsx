015fc1ccc85477bdd349dee3867dc8506e6cfb51
chrystophermedeiros <chrystopher312@gmail.com>
2023-12-14 00:09:19 -0300

    Refactor: melhorias nos trabalhos.

--- src/assets/data/work.jsx
+++ src/assets/data/work.jsx
@@ -15,10 +15,9 @@ export const work = [
     enddate: currentDate,
 
     techs: [
-      { id: 1, name: 'HTML', title: skills[0].title, img: skills[0].img },
-      { id: 2, name: 'CSS', title: skills[1].title, img: skills[1].img },
+      { id: 1, name: 'React', title: skills[4].title, img: skills[4].img },
+      { id: 2, name: 'TypeScript', title: skills[3].title, img: skills[3].img },
       { id: 3, name: 'JavaScript', title: skills[2].title, img: skills[2].img },
-      { id: 4, name: 'React', title: skills[4].title, img: skills[4].img },
     ],
   },
 
